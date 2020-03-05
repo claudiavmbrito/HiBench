7644757805e5ec2b2563a554ed7fa8bbcdd70851
user Cláudia Brito <cvmb.mat@gmail.com>
2020-03-05 10:18:44 +0000

    small changes

--- bin/workloads/ml/linear/prepare/prepare.sh
+++ bin/workloads/ml/linear/prepare/prepare.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+
 current_dir=`dirname "$0"`
 current_dir=`cd "$current_dir"; pwd`
 root_dir=${current_dir}/../../../../../
